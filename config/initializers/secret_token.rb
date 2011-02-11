441943eaea72bb5a1b4d234fd7b36d862fb2e072
Luke Harrison <luke@archcreate.com>
2011-02-11 22:24:16 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrialApp::Application.config.secret_token = 'df021d5b45ebacf498e3d94df70b2592c564923ded24210009be359b1bdae52d32dbf82b5a7726111591530e55fbbf4121bb52c76e59f2fe8c9d579ca13694bd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TrialApp::Application.config.session_store :cookie_store, :key => '_trial_app_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TrialApp::Application.config.session_store :active_record_store
