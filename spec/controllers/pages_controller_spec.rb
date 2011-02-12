db344429a85f1b6d9b5ea9eecb36589fd2de425c
Luke Harrison <luke@archcreate.com>
2011-02-12 22:40:46 +0000

    DRYed up the pages spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
   render_views
+  
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Trial App"
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,8 +15,8 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Trial App | Home")
+      response.should have_selector("title", 
+                                    :content => @base_title + " | Home")
     end
   end
   
@@ -26,7 +30,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Trial App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
   
@@ -39,7 +43,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Trial App | About")
+                                    :content => @base_title + " | About")
     end
   end
   
@@ -52,7 +56,7 @@ describe PagesController do
     it "should have the right title" do
       get 'help'
       response.should have_selector("title",
-                      :content => "Ruby on Rails Tutorial Trial App | Help")
+                                    :content => @base_title + " | Help")
     end
   end
 
